49e266208800422aad84a87e30b4f3680cd3f290
ryan10projects <rodricksryan10@gmail.com>
2022-06-18 17:54:17 +0530

    files pushed

--- src/Config.js
+++ src/Config.js
@@ -5,13 +5,7 @@ import firebase from 'firebase/compat/app';
 
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDxJIOgZ8kmw3QKwrN5SLiyjeCwKR57rjc",
-    authDomain: "chat-app-3a1ab.firebaseapp.com",
-    projectId: "chat-app-3a1ab",
-    storageBucket: "chat-app-3a1ab.appspot.com",
-    messagingSenderId: "219485355770",
-    appId: "1:219485355770:web:44ce8a28e48b7f7f6c9343",
-    measurementId: "G-SXZ652MGKZ"
+
 };
 
 
@@ -23,3 +17,4 @@ const firestore = getFirestore(app);
 export { auth, firestore, firebase, db };
 
     
+
